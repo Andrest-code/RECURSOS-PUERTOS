5d42108450a3aa80fd2e59149ddc60769a5b91e6
Andrest-code <atorresq.coding@gmail.com>
2025-08-22 14:55:15 -0500

    Update PUERTOS.py

--- PUERTOS.py
+++ PUERTOS.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
 import sys
 import socket
 from datetime import datetime
-
-# Obtener el banner del servicio
 def grab_banner(ip, port):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -15,25 +12,21 @@ def grab_banner(ip, port):
         return "No se pudo obtener el banner"
     except Exception as e:
         return f"Error: {e}"
-
 if len(sys.argv) == 2:
     target = socket.gethostbyname(sys.argv[1])
 else:
     print("Argumentos no válida.")
     print("Sintaxis: python3 port_scanner.py <ip>")
     sys.exit(1)
-
 print("-" * 50)
 print(f"Escaneando objetivo: {target}")
 print(f"Tiempo de inicio: {datetime.now()}")
 print("-" * 50)
-
 start_time = datetime.now()
 open_ports = 0
-
 try:
     socket.setdefaulttimeout(1)
-    for port in range(1, 1024):  # Escaneo de puertos comunes
+    for port in range(1, 1024):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             result = s.connect_ex((target, port))
             if result == 0:
@@ -50,11 +43,10 @@ except socket.gaierror:
 except socket.error:
     print("\n[!] No se puede conectar al servidor.")
     sys.exit()
-
 end_time = datetime.now()
 total_time = end_time - start_time
-
 print("\n" + "-" * 50)
 print(f"Escaneo completado en: {total_time}")
 print(f"Puertos abiertos encontrados: {open_ports}")
 print("-" * 50)
+
